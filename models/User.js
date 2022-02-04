dd4a40c6d360634a96299186af8e99ae3991cbf3
David Schepis <davidschepis@gmail.com>
2022-02-03 19:34:12 -0700

    created user model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Invalid Email"]
+    },
+    thoughts: {
+        type: Array,
+        ref: "Thought"
+    },
+    friends: {
+        type: Array,
+        ref: "User"
+    }
+});
+
+userSchema.virtual('friendCount').get(() => {
+    return friends.length;
+});
